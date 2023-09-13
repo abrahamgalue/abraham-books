6057b8897a86b58f1924f5dff8d0ed0418574acc
Abraham Galue <abrahamgalue512@gmail.com>
2023-09-12 20:53:31 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# Astro: Abraham Books
+
+### 👀 ¿Qué es este proyecto?
+
+Es una lista de mis libros favoritos utilizando Astro como librería y Tailwind.
+
+Como punto a destacar implementa las [View Transitions API](https://developer.chrome.com/docs/web-platform/view-transitions/) de forma vanilla implementada por Chrome.
---
+++ src/data/books.js
@@ -0,0 +1,82 @@
+export const books = [
+  {
+    id: "the-shadow-of-the-wind",
+    title: "The Shadow of the Wind",
+    img: "https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1628791882i/1232.jpg",
+    opinion:
+      "Barcelona, 1945: A city slowly heals from its war wounds, and Daniel, an antiquarian book dealer's son who mourns the loss of his mother, finds solace in a mysterious book entitled The Shadow of the Wind, by one Julian Carax. But when he sets out to find the author's other works, he makes a shocking discovery: someone has been systematically destroying every copy of every book Carax has written. In fact, Daniel may have the last of Carax's books in existence. Soon Daniel's seemingly innocent quest opens a door into one of Barcelona's darkest secrets--an epic story of murder, madness, and doomed love.",
+    author: "Carlos Ruiz Zafón",
+  },
+  {
+    id: "the-end-of-eternity",
+    title: "The End of Eternity",
+    img: "https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1405527675i/509784.jpg",
+    opinion:
+      "Andrew Harlan is an Eternal, a man whose job it is to range through past and present Centuries, monitoring and, where necessary, altering Time's myriad cause-and-effect relationships. But when Harlan meets and falls for a non-Eternal woman, he seeks to use the awesome powers and techniques of the Eternals to twist time for his own purposes, so that he and his love can survive together.",
+    author: "Isaac Asimov",
+  },
+  {
+    id: "hamlet",
+    title: "Hamlet",
+    img: "https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1351051208i/1420.jpg",
+    opinion:
+      'Among Shakespeare\'s plays, "Hamlet" is considered by many his masterpiece. Among actors, the role of Hamlet, Prince of Denmark, is considered the jewel in the crown of a triumphant theatrical career. Now Kenneth Branagh plays the leading role and co-directs a brillant ensemble performance. Three generations of legendary leading actors, many of whom first assembled for the Oscar-winning film "Henry V", gather here to perform the rarely heard complete version of the play. This clear, subtly nuanced, stunning dramatization, presented by The Renaissance Theatre Company in association with "Bbc" Broadcasting, features such luminaries as Sir John Gielgud, Derek Jacobi, Emma Thompson and Christopher Ravenscroft. It combines a full cast with stirring music and sound effects to bring this magnificent Shakespearen classic vividly to life. Revealing new riches with each listening, this production of "Hamlet" is an invaluable aid for students, teachers and all true lovers of Shakespeare - a recording to be treasured for decades to come.',
+    author: "William Shakespeare",
+  },
+  {
+    id: "12-rules-for-life",
+    title: "12 Rules for Life: An Antidote to Chaos",
+    img: "https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1512705866i/30257963.jpg",
+    opinion:
+      "What does everyone in the modern world need to know? Renowned psychologist Jordan B. Peterson's answer to this most difficult of questions uniquely combines the hard-won truths of ancient tradition with the stunning revelations of cutting-edge scientific research. Humorous, surprising, and informative, Dr. Peterson tells us why skateboarding boys and girls must be left alone, what terrible fate awaits those who criticize too easily, and why you should always pet a cat when you meet one on the street. What does the nervous system of the lowly lobster have to tell us about standing up straight (with our shoulders back) and about success in life? Why did ancient Egyptians worship the capacity to pay careful attention as the highest of gods? What dreadful paths do people tread when they become resentful, arrogant, and vengeful? Dr. Peterson journeys broadly, discussing discipline, freedom, adventure, and responsibility, distilling the world's wisdom into 12 practical and profound rules for life. 12 Rules for Life shatters the modern commonplaces of science, faith, and human nature while transforming and ennobling the mind and spirit of its listeners.",
+    author: "Jordan B. Peterson",
+  },
+  {
+    id: "beyond-order",
+    title: "Beyond Order: 12 More Rules For Life",
+    img: "https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1675973189i/56019043.jpg",
+    opinion:
+      "The sequel to 12 Rules for Life offers further guidance on the periolus path of modern life. In 12 Rules for Life, clinical psychologist and celebrated professor at Harvard and the University of Toronto Dr. Jordan B. Peterson helped millions of readers impose order on the chaos of their lives. Now, in this bold sequel, Peterson delivers twelve more lifesaving principles for resisting the exhausting toll that our desire to order the world inevitably takes. In a time when the human will increasingly imposes itself over every sphere of life—from our social structures to our emotional states—Peterson warns that too much security is dangerous. What’s more, he offers strategies for overcoming the cultural, scientific, and psychological forces causing us to tend toward tyranny, and teaches us how to rely instead on our instinct to find meaning and purpose, even—and especially—when we find ourselves powerless. While chaos, in excess, threatens us with instability and anxiety, unchecked order can petrify us into submission. Beyond Order provides a call to balance these two fundamental principles of reality itself, and guides us along the straight and narrow path that divides them.",
+    author: "Jordan B. Peterson",
+  },
+  {
+    id: "the-selfish-gene",
+    title: "The Selfish Gene",
+    img: "https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1366758096i/61535.jpg",
+    opinion:
+      '"The Selfish Gene" caused a wave of excitement among biologists and the general public when it was first published in 1976. Its vivid rendering of a gene\'s eye view of life, in lucid prose, gathered together the strands of thought about the nature of natural selection into a conceptual framework with far-reaching implications for our understanding of evolution. Time has confirmed its significance. Intellectually rigorous, yet written in non-technical language, "The Selfish Gene" is widely regarded as a masterpiece of science writing, and its insights remain as relevant today as on the day it was published.',
+    author: "Richard Dawkins",
+  },
+  {
+    id: "how-the-catholic-church-built-western-civilization",
+    title: "How the Catholic Church Built Western Civilization",
+    img: "https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1391996944i/313011.jpg",
+    opinion:
+      'Ask a college student today what he knows about the Catholic Church and his answer might come down to one word: "corruption." But that one word should be "civilization." Western civilization has given us the miracles of modern science, the wealth of free-market economics, the security of the rule of law, a unique sense of human rights and freedom, charity as a virtue, splendid art and music, a philosophy grounded in reason, and innumerable other gifts that we take for granted as the wealthiest and most powerful civilization in history. But what is the ultimate source of these gifts? Bestselling author and professor Thomas E. Woods, Jr. provides the long neglected answer: the Catholic Church. Woods’s story goes far beyond the familiar tale of monks copying manuscripts and preserving the wisdom of classical antiquity. In How the Catholic Church Built Western Civilization, you’ll learn: · Why modern science was born in the Catholic Church · How Catholic priests developed the idea of free-market economics five hundred years before Adam Smith · How the Catholic Church invented the university · Why what you know about the Galileo affair is wrong · How Western law grew out of Church canon law · How the Church humanized the West by insisting on the sacredness of all human life No institution has done more to shape Western civilization than the two-thousand-year-old Catholic Church—and in ways that many of us have forgotten or never known. How the Catholic Church Built Western Civilization is essential reading for recovering this lost truth.',
+    author: "Thomas E. Woods Jr.",
+  },
+  {
+    id: "perfume",
+    title: "Perfume: The Story of a Murderer",
+    img: "https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1409112276i/343.jpg",
+    opinion:
+      "An acclaimed bestseller and international sensation, Patrick Suskind's classic novel provokes a terrifying examination of what happens when one man's indulgence in his greatest passion—his sense of smell—leads to murder. In the slums of eighteenth-century France, the infant Jean-Baptiste Grenouille is born with one sublime gift—an absolute sense of smell. As a boy, he lives to decipher the odors of Paris, and apprentices himself to a prominent perfumer who teaches him the ancient art of mixing precious oils and herbs. But Grenouille's genius is such that he is not satisfied to stop there, and he becomes obsessed with capturing the smells of objects such as brass doorknobs and fresh-cut wood. Then one day he catches a hint of a scent that will drive him on an ever-more-terrifying quest to create the \"ultimate perfume\"—the scent of a beautiful young virgin. Told with dazzling narrative brilliance, Perfume is a hauntingly powerful tale of murder and sensual depravity.",
+    author: "Patrick Süskind",
+  },
+  {
+    id: "the-gulag-archipelago",
+    title: "The Gulag Archipelago 1918–1956",
+    img: "https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1388293026i/70561.jpg",
+    opinion:
+      "Drawing on his own incarceration and exile, as well as on evidence from more than 200 fellow prisoners and Soviet archives, Aleksandr I. Solzhenitsyn reveals the entire apparatus of Soviet repression—the state within the state that ruled all-powerfully. Through truly Shakespearean portraits of its victims—men, women, and children—we encounter secret police operations, labor camps and prisons; the uprooting or extermination of whole populations, the welcome that awaited Russian soldiers who had been German prisoners of war. Yet we also witness the astounding moral courage of the incorruptible, who, defenseless, endured great brutality and degradation. The Gulag Archipelago 1918–1956—a grisly indictment of a regime, fashioned here into a veritable literary miracle—has now been updated with a new introduction that includes the fall of the Soviet Union and Solzhenitsyn's move back to Russia.",
+    author: "Aleksandr Solzhenitsyn",
+  },
+  {
+    id: "the-rape-of-nanking",
+    title: "The Rape of Nanking: The Forgotten Holocaust of World War II",
+    img: "https://m.media-amazon.com/images/I/51jhXAxdZYL.jpg",
+    opinion:
+      "In December 1937, the Japanese army invaded the ancient city of Nanking, systematically raping, torturing, and murdering more than 300,000 Chinese civilians. This book tells the story from three perspectives: of the Japanese soldiers who performed it, of the Chinese civilians who endured it, and of a group of Europeans and Americans who refused to abandon the city and were able to create a safety zone that saved many.",
+    author: "Iris Chang",
+  },
+];
